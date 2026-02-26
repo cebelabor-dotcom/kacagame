4a362a4da6bf377d5e3219be2d7a177a29383491
cebelabor-dotcom <cebela.bor@gmail.com>
2026-02-26 19:21:32 +0100

    Add files via upload

--- kaca.py
+++ kaca.py
@@ -19,6 +19,7 @@
 #naredi megre obeh branchov 
 
 import pygame
+import random
 
 pygame.init()
 
@@ -36,13 +37,24 @@ velikost = 50
 
 smer = "right"
 
+dolzina_kaca = 1
+
+barve = ((30, 17, 184), (240, 193, 53), (56, 14, 52), (232, 46, 214))
+
 clock = pygame.time.Clock()
 
 exit = False
+
+sadje = []
+
+stev_sadje = 0
+
 while not exit:
 	clock.tick(5)
 
 	canvas.fill((0, 0, 0))
+
+	obrat = False
 
 	for a in range(0, 1000, velikost):
 		for b in range (0, 1000, velikost):
@@ -51,20 +63,45 @@ while not exit:
 			else:
 				pygame.draw.rect(canvas, barva2, (a, b, velikost, velikost))
 
+	for a in range(3):
+		if stev_sadje != 3:
+			sadje_x = random.randint(0, 20)*50
+			sadje_y = random.randint(0, 20)*50
+			nou = True
+			if sadje_x in sadje and sadje_y in sadje:
+				sadje.append((sadje_x, sadje_y, random.choice(barve)))
+			else:
+				while nou:
+					sadje_x = random.randint(0, 20)*50
+					sadje_y = random.randint(0, 20)*50
+					if sadje_x in sadje and sadje_y in sadje:
+						sadje.append((sadje_x, sadje_y, random.choice(barve)))
+						nou = False
+
+			stev_sadje += 1
+
+	for b in range(len(sadje)):
+		pygame.draw.rect(canvas, sadje[b][2], (sadje[b][0], sadje[b][1], velikost, velikost))
+
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			exit = True
 
 	
 		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_a and smer != "right":
+			if event.key == pygame.K_a and smer != "right" and obrat != True:
 				smer = "left"
-			elif event.key == pygame.K_d and smer != "left":
+				obrat = True
+			elif event.key == pygame.K_d and smer != "left" and obrat != True:
 				smer = "right"
-			elif event.key == pygame.K_w and smer != "down":
+				obrat = True
+			elif event.key == pygame.K_w and smer != "down" and obrat != True:
 				smer = "up"
-			elif event.key == pygame.K_s and smer != "up":
+				obrat = True
+			elif event.key == pygame.K_s and smer != "up" and obrat != True:
 				smer = "down"
+				obrat = True
 
 	if smer == "right":
 		x += 50
@@ -74,6 +111,7 @@ while not exit:
 		y -= 50
 	elif smer == "down":
 		y += 50
+
 
 	kvadrat = pygame.Rect(x, y, 50, 50)
 
@@ -87,6 +125,17 @@ while not exit:
 			if event.type == pygame.QUIT:
 				exit = True
 				game_over = False
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_r:
+					exit = False
+					game_over = False
+					x = 50
+					y = 50
+					smer = "right"
+					obrat = False
+					dolzina_kaca = 1
+					sadje = []
+					stev_sadje = 0
 
 
 	pygame.draw.rect(canvas, (200, 0, 0), kvadrat)
